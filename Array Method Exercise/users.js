984e900a302b644a2945c58c04046e1292c0803b
fmartinez1299 <130628777+fmartinez1299@users.noreply.github.com>
2024-11-13 14:58:38 -0800

    Add files via upload

---
+++ Array Method Exercise/users.js
@@ -0,0 +1,5 @@
+[
+	{ fullName: "Alice Johnson", membershipStatus: "Premium" },
+	{ fullName: "Bob Smith", membershipStatus: "Standard" },
+	{ fullName: "Charlie Brown", membershipStatus: "Premium" }
+]
